1f3fcd5456188e4ea75a7e27944b4d6a25cfea18
Kasin Arvonen <kasinarvonen@gmail.com>
2023-01-18 18:19:43 -0700

    class program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Random generator = new Random():
+        int magicNumber = generator.Next(0, 10):
+
+        bool keepPlaying = true
+
+        while(keepPlaying) {
+            Console.Write("Guess a number between 1 and 10: ")
+            int userGuess = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine(userGuess);
+            Console.WriteLine(magicNumber);
+        }
+
+        string userGuess = Console.ReadLine() ?? "";
+
+            if (magicNumber == userGuess) {
+                Console.WriteLine("Nice! You guessed it!");
+                keepPlaying = false;
+                }
+                else if (magicNumber > userGuess) {
+                    Console.WriteLine ("Higher");
+                else {
+                    Console.WriteLine("Lower");
+                }
+                }
+                }
+
+        }
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job_one = new Job();
+        job_one.jobTitle = "Software Engineer";
+        job_one.company = "Microsoft";
+        job_one.startYear = 2019;
+        job_one.endYear =  2022;
+
+        Job job_two = new Job();
+        job_two.jobTitle = "Manager"
+        job_two.company = "Apple"
+        job_two.startYear = 2022
+        job_two.endYear = 2023
+
+        Resume myResume = new Resume();
+        myResume.name = "Allison Rose";
+
+        myResume.jobs.Add(job_one);
+        myResume.job.Add(job_two);
+
+        myResume.Display();
     }
+}
+
+class Job{
+   public string company;
+   public string jobTitle;
+
+}
+
+class Resume{
+    public string name;
+
+    public List<Job> _job = new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _job)
+        {job.Display();
+            
+        }
+    }
+
+
+
 }
