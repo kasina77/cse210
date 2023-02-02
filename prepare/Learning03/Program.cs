058014f334a6802b32769cf35abc3eec57b423a9
Kasin Arvonen <kasinarvonen@gmail.com>
2023-02-01 20:22:05 -0700

    fraction assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,3 +7,50 @@ class Program
         Console.WriteLine("Hello Learning03 World!");
     }
 }
+
+class Fraction()
+{
+    private int top;
+    
+    private int bottom;
+
+    public Fraction()
+    {
+        top = 1;
+        bottom = 1;
+
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        top = wholeNumber;
+
+        bottom = 1;
+        
+
+    }
+
+    public Fraction(int topNum, int bottomNum)
+    {
+        top = topNum;
+
+        bottom = bottomNum;
+    }
+
+    public GetFractionString()
+    {
+       string text = $'{top}/{bottom}';
+       
+       return text;
+
+    }
+
+    public GetDecimalValue()
+    {
+        return (double)top / (double)bottom;
+
+    }
+    
+
+
+}
